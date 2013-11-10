d9b8445c693bd004f88e367d35c1cdcba5b7261c
Bjoern Ahlfeld <bjoern.ahlfeld@project-a.com>
2013-11-10 18:22:20 +0100

    Registration controller

--- app/controllers/Registration.java
+++ app/controllers/Registration.java
@@ -9,35 +9,35 @@ import views.html.registrationForm;
 
 public class Registration extends Controller {
 
-	public static Result index() {
-		return ok(registrationForm.render(Form.form(Registration.class)));
-	}
+    public static Result index() {
+        return ok(registrationForm.render(Form.form(Registration.class)));
+    }
 
-	public static Result validate() {
-		Form<User> registrationForm = Form.form(User.class).bindFromRequest();
-		if (registrationForm.hasErrors()) {
-			return redirect(routes.Application.index());
-		} else {
-			addUserToDB(generateUserObject());
-			sendRegConfirmationMail();
-			return redirect(routes.Application.index());
-		}
-			
-	}
+    public static Result validate() {
+        Form<User> registrationForm = Form.form(User.class).bindFromRequest();
+        if (registrationForm.hasErrors()) {
+            return redirect(routes.Application.index());
+        } else {
+            addUserToDB(generateUserObject());
+            sendRegConfirmationMail();
+            return redirect(routes.Application.index());
+        }
+            
+    }
 
-	public static Result submit() {
-		return ok();
-	}
+    public static Result submit() {
+        return ok();
+    }
 
-	private static User generateUserObject() {
-		return new User();
-	}
+    private static User generateUserObject() {
+        return new User();
+    }
 
-	private static void addUserToDB(User newUser) {
-		//TODO
-	}
+    private static void addUserToDB(User newUser) {
+        //TODO
+    }
 
-	private static void sendRegConfirmationMail() {
-		//TODO
-	}
+    private static void sendRegConfirmationMail() {
+        //TODO
+    }
 }
