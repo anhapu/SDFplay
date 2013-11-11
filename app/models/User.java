a6666ae1d674dc94a4a1053b280531d2f6b55937
Jensus <jensusberlin@gmail.com>
2013-11-11 17:08:49 +0100

    Moved md5 Method from Usermodel to Common Class

--- app/models/User.java
+++ app/models/User.java
@@ -95,30 +95,5 @@ public class User extends Model
     public String toString() {
         return "User(id:" + id + " " + email + ")";
     }
-
-    /**
-     * Generates an md5 hash of a String.
-     * @param input String value
-     * @return Hashvalue of the String.
-     */
-    public static String md5(String input) {
-        
-        String md5 = null;
-        if(null == input) return null;
-        
-	        try {
-	        //Create MessageDigest object for MD5
-	        MessageDigest digest = MessageDigest.getInstance("MD5");
-	        //Update input string in message digest
-	        digest.update(input.getBytes(), 0, input.length());
-	        //Converts message digest value in base 16 (hex) 
-	        md5 = new BigInteger(1, digest.digest()).toString(16);
-	
-	        } catch (NoSuchAlgorithmException e) {
-	            e.printStackTrace();
-	        }
-	        
-        return md5;
-    }
     
 }
