4da4f3dc1ab84f880c4655223e1098d96abd4d96
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2013-11-20 17:03:09 +0100

    email template

--- app/controllers/EmailSender.java
+++ app/controllers/EmailSender.java
@@ -12,7 +12,11 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import models.User;
 import controllers.SMTPAuthenticator;
+import views.html.email.forgotPassword;
+import views.html.email.registration;
+import views.html.email.exchangeRequest;
 
 /** Ein Objekt zum Versand von Emails.
  */
@@ -22,7 +26,7 @@ public class EmailSender {
 	 */
 	private EmailSender() {
 	}
-		
+	
 	/** Versendet eine E-Mail mit dem angegebenen Betreff und Inhalt an die gewuenschte Ziel-E-Mail-Adresse.
 	 *  Der Absender ist buecher.boerse@gmx.de. 
 	 * 
@@ -32,7 +36,7 @@ public class EmailSender {
 	 */
 	public static void send(String subject, String message, String to) {
 		Properties props = prepareProperties();
-	    Session mailSession = Session.getDefaultInstance(props, new SMTPAuthenticator(props.getProperty("mail.smtp.user"), props.getProperty("mail.smtp.password"), true));
+	    Session mailSession = Session.getInstance(props, new SMTPAuthenticator(props.getProperty("mail.smtp.user"), props.getProperty("mail.smtp.password"), true));
 	    try {
 		    Message msg = new MimeMessage(mailSession);
 		    msg.setFrom(new InternetAddress(props.getProperty("mail.smtp.user")));
@@ -84,7 +88,38 @@ public class EmailSender {
 		}
 	}
 
-
+	/** Versendet eine E-Mail zur Bestaetigung der Registrierung an den Nutzer.
+	 *  Der Absender ist buecher.boerse@gmx.de.
+	 *  
+	 * @param user			Nutzer, der sich registriert hat.
+	 */ 
+	public static void sendRegistration(User user) {
+		String message = registration.render(user.username).toString();
+		send("Registrierung erfolgreich.", message, user.email);
+	}
+	
+	/** Versendet eine E-Mail mit einem book exchange request an einen Nutzer.
+	 *  Der Absender ist buecher.boerse@gmx.de.
+	 *  
+	 * @param fromUser		Nutzer, welcher den exchange request gestellt hat.
+	 * @param toUser		Nutzer, an den dieser exchange request gerichtet ist.
+	 */
+	public static void sendBookExchangeRequest(User fromUser, User toUser) {
+		String message = exchangeRequest.render(fromUser.username, toUser.username).toString();
+		send("Neue Tauschanfrage von " + fromUser.username, message, toUser.email);
+	}
+	
+	/** Sendet eine E-Mail mit einem Link zu Resetten des Passworts.
+	 *  Der Absender ist buecher.boerse@gmx.de.
+	 * 
+	 * @param user			Nutzer, der sein Passwort resetten moechte.
+	 * @param linkAddress	Link, der auf eine Seite zum Reset des Passworts fuehrt.
+	 */
+	public static void sendForgotPassword(User user, String linkAddress) {
+		String message = forgotPassword.render(user.username, linkAddress).toString();
+		send("Reset Ihres Passworts..", message, user.email);
+	}
+	
 	/** Liefert Objekt vom Typ Properties, welches mit den Nutzerdaten der E-Mail-Adresse von buecher.boerse@gmx.de 
 	 * 	initialisiert wurde.
 	 * 
@@ -99,6 +134,7 @@ public class EmailSender {
 	    props.setProperty("mail.smtp.password", "#123#abc#");
 	    props.setProperty("mail.smtp.auth", "true");
 	    props.setProperty("mail.debug", "true");
+	    props.setProperty("mail.smtp.ssl.enable", "true");
 	    props.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
        return props;
 	}	
