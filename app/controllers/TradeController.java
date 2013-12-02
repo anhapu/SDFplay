1ff7bc3e93023f4fda0811c924c5dd72edb3215e
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2013-12-02 08:29:19 +0100

    no exception thrown anymore. added flash message instead.

--- app/controllers/TradeController.java
+++ app/controllers/TradeController.java
@@ -29,39 +29,42 @@ public class TradeController extends Controller {
     	
     	User owner = Common.currentUser();
     	User recipient = User.findById(recipientId);
-    	
+
     	//Check if transaction already exists.
     	Logger.info("A TradeTransaction for (owner = " + owner.username + " ,recipient = " + recipient.username + ") already exists? " + TradeTransaction.exists(owner, recipient).toString().toUpperCase());
-    	
-    	// Create the transaction
-    	TradeTransaction trade = new TradeTransaction();
-    	trade.owner = owner;
-    	trade.recipient = recipient;
-    	trade.state = States.INIT;
-    	trade.commentOwner = "Hallo! Wie waere es mit einem Buchtausch?";
-    	trade.commentRecipient = "Ok. Geht klar.";
-    	trade.save();
-    	
-    	// Getting the selection
-    	String[] bookSelection = request().body().asFormUrlEncoded().get("book_selection");
-    	if(bookSelection == null) {
-    		flash("error", "Bitte wähle min. ein Buch aus.");
-    		return redirect(routes.BookController.showBookshelf(recipientId));
+    	if (TradeTransaction.exists(owner, recipient)) {
+    		flash("error", "Dieser Wunschzettel existiert bereits und konnte nicht neu angelegt werden.");
+    	} else {
+	    	// Create the transaction
+	    	TradeTransaction trade = new TradeTransaction();
+	    	trade.owner = owner;
+	    	trade.recipient = recipient;
+	    	trade.state = States.INIT;
+	    	trade.commentOwner = "Hallo! Wie waere es mit einem Buchtausch?";
+	    	trade.commentRecipient = "Ok. Geht klar.";
+	    	trade.save();
+	    	
+	    	// Getting the selection
+	    	String[] bookSelection = request().body().asFormUrlEncoded().get("book_selection");
+	    	if(bookSelection == null) {
+	    		flash("error", "Bitte wähle min. ein Buch aus.");
+	    		return redirect(routes.BookController.showBookshelf(recipientId));
+	    	}
+	    	
+	 		Long bookId = null;
+	 		Book book = null;
+	 		for (String bookString : bookSelection) {
+	    		bookId = Long.parseLong(bookString);
+	    		book = Book.findById(bookId);
+	    		TradeBooks tradeBook = new TradeBooks();
+	    		tradeBook.book = book;
+	    		tradeBook.tradeTransaction = trade;
+	    		tradeBook.save();
+				Logger.info("Added Book " + book.id.toString() + " to Transaction " + trade.id.toString());
+			}
+	    	
+	 		flash("success", "Wunschzettel angelegt");
     	}
-    	
- 		Long bookId = null;
- 		Book book = null;
- 		for (String bookString : bookSelection) {
-    		bookId = Long.parseLong(bookString);
-    		book = Book.findById(bookId);
-    		TradeBooks tradeBook = new TradeBooks();
-    		tradeBook.book = book;
-    		tradeBook.tradeTransaction = trade;
-    		tradeBook.save();
-			Logger.info("Added Book " + book.id.toString() + " to Transaction " + trade.id.toString());
-		}
-    	
- 		flash("success", "Wunschzettel angelegt");
     	return redirect(routes.Application.index());
     }
 	
