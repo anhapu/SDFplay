3c5ae064238bae4395acf2171269d0da0b79d386
Jensus <jensusberlin@gmail.com>
2013-12-04 10:19:10 +0100

    Added Drag&Drop

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,32 @@
+$(document).ready(function() {
+	$('.book').draggable({
+		revert: "invalid",
+		helper: "clone",
+		start: function() {
+			$(this).fadeOut(0);
+		},
+		stop: function() {
+			$(this).fadeIn(0);
+		}
+	});
+
+	$('#showcase').droppable({
+		accept: "#wishlist > .book",
+		drop: function(event, ui) {
+			ui.draggable.removeClass("inList").fadeIn(1);
+			$('#' + ui.draggable[0].id).appendTo('#showcase');
+			$('#wishlist').remove('#' + ui.draggable[0].id);
+		}
+	});
+
+	$('#wishlist').droppable({
+		accept: "#showcase > .book",
+		drop: function(event, ui) {
+			ui.draggable.addClass("inList").fadeIn(1);
+			$('#' + ui.draggable[0].id).appendTo('#wishlist');
+			$('#showcase').remove('#' + ui.draggable[0].id);
+		}
+	});
+});
+
 $('[data-toggle="popover"]').popover('hide');
