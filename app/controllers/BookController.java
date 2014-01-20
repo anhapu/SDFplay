anhapu/SDFplay

commit 71200e4e99c0a9faad10197906c391106ea2ed79

Added book to createBook.scala.html

app/controllers/BookController.java
@@ -44,7 +44,7 @@ public final class BookController extends Controller {
     @Security.Authenticated(Secured.class)
     public static Result getForm() {
         String navigation = "addBook";
-        return ok(views.html.book.createBook.render(bookForm, navigation));
+        return ok(views.html.book.createBook.render(null, bookForm, navigation));
     }
     
     @Transactional
@@ -58,12 +58,12 @@ public final class BookController extends Controller {
         filledForm.errors().remove("tradeable");
         
         if(filledForm.hasErrors()) {
-            return badRequest(views.html.book.createBook.render(filledForm, navigation));
+            return badRequest(views.html.book.createBook.render(filledForm.get(), filledForm, navigation));
         } else {
             
-        	final SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-MM-dd" );
+            final SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-MM-dd" );
             final SimpleDateFormat formatter2 = new SimpleDateFormat("yyyy");
-        	
+            
             Book book = new Book();
             book.owner = Common.currentUser();
             book.tradeable = false;
@@ -74,15 +74,15 @@ public final class BookController extends Controller {
             book.author = filledForm.field("author").value();
             book.isbn = Utils.isbnParser(filledForm.field("isbn").value());
             try{
-            	book.year = formatter.parse(filledForm.field("year").value());
+                book.year = formatter.parse(filledForm.field("year").value());
             }catch (final ParseException e){
-            	Logger.error(e.getMessage());
-            	try {
-            		book.year = formatter2.parse(filledForm.field("year").value());
-            	} catch ( final ParseException e2) {
-            		Logger.error(e2.getMessage());
-            		book.year = new Date();
-            	}
+                Logger.error(e.getMessage());
+                try {
+                    book.year = formatter2.parse(filledForm.field("year").value());
+                } catch ( final ParseException e2) {
+                    Logger.error(e2.getMessage());
+                    book.year = new Date();
+                }
             }
             
             book.coverUrl = filledForm.field("coverUrl").value();
@@ -152,12 +152,12 @@ public final class BookController extends Controller {
         } else {
             Book book = Utils.getBookInformationFromAWS(Utils.isbnParser(pForm.get().isbn));
             if (book == null) {
-            	flash("error", "Das Buch konnte nicht korrekt ermittelt werden, bitte füge es per Hand hinzu!");
+                flash("error", "Das Buch konnte nicht korrekt ermittelt werden, bitte füge es per Hand hinzu!");
                return ok(views.html.book.addBook.render(form(SimpleProfile.class), navigation));
             } else {
-            	flash("info", "Dein Buch wurde gefunden! Bitte überprüfe die Angaben und ergänze sie gegebenenfalls.");
+                flash("info", "Dein Buch wurde gefunden! Bitte überprüfe die Angaben und ergänze sie gegebenenfalls.");
             }
-            return ok(views.html.book.createBook.render(bookForm.fill( book ), navigation));
+            return ok(views.html.book.createBook.render(book, bookForm.fill( book ), navigation));
         }
     }
  
@@ -188,14 +188,14 @@ public final class BookController extends Controller {
     /** Delete a book. This action will also lead to a search for INVALID TradeTransactions. If this
      * 	book is used in a TradeTransaction, then this TradeTransaction will become INVALID. For each
      * 	invalid TradeTransaction one E-Mail is sent to the owner and another one is sent to the recipient.
-     * 
+     *
      * 	@param bookId	the id of the book that you want to delete
-     */ 
+     */
     @Security.Authenticated(Secured.class)
     public static Result deleteBook(final Long bookId) {
         Book book = Book.findById(bookId);
         if (Secured.isOwnerOfBook(book)) {
-        	checkForInvalidTradeTransactions(book);
+            checkForInvalidTradeTransactions(book);
             book.delete();
             return redirect(routes.BookController.myBookshelf());
         } else {
@@ -273,7 +273,7 @@ public final class BookController extends Controller {
     public static Result unmarkAsTradeable(final Long bookId) {
         Book book = Book.findById(bookId);
         if (Secured.isOwnerOfBook(book)) {
-        	checkForInvalidTradeTransactions(book);
+            checkForInvalidTradeTransactions(book);
             Book.unmarkAsTradeable(book);
             return redirect(routes.BookController.myBookshelf());
 
@@ -389,7 +389,7 @@ public final class BookController extends Controller {
                    sortDirection = data[1];
               }
          }
-         // "" to get all books 
+         // "" to get all books
          books = Book.findAllBooksFromBy(Common.currentUser(), "", sortAttribute, sortDirection);
          return ok(views.html.book.mybookshelf.render(books, sortAttribute, sortDirection, navigation));
      }
@@ -397,25 +397,25 @@ public final class BookController extends Controller {
     /** This method should be used, if a user deleted a book or removed it from his or her "showcase".
      * 	It will find all TradeTransaction involved and set them to "States.INVALID". It will also sent
      *  notifications to all users involved
-     * 
+     *
      * @param book A book that was deleted or set to to be "untradeable".
      */
     private static void checkForInvalidTradeTransactions(Book book) {
-    	List<TradeTransaction> invalidTradeTransactions = TradeTransaction.findListOfTradeTransactionInvolvedInBook(book);
-    	if (invalidTradeTransactions != null) {
-    		List<Email> emailList = new ArrayList<Email>();
-			for (TradeTransaction invalidTradeTransaction : invalidTradeTransactions) {
-				// set State to INVALID only, if trade is not finished yet (INIT or RESPONSE)
-				if ((invalidTradeTransaction.state == States.INIT) || (invalidTradeTransaction.state == States.RESPONSE)) {
-					invalidTradeTransaction.state = States.INVALID;
-					invalidTradeTransaction.save();
-					emailList.addAll(EmailSender.getBookExchangeInvalid(invalidTradeTransaction.owner, invalidTradeTransaction.recipient));
-				}
-			}
-			if (!emailList.isEmpty()) {
-				EmailSender.send(emailList);
-			}
-    	}
+        List<TradeTransaction> invalidTradeTransactions = TradeTransaction.findListOfTradeTransactionInvolvedInBook(book);
+        if (invalidTradeTransactions != null) {
+            List<Email> emailList = new ArrayList<Email>();
+            for (TradeTransaction invalidTradeTransaction : invalidTradeTransactions) {
+                // set State to INVALID only, if trade is not finished yet (INIT or RESPONSE)
+                if ((invalidTradeTransaction.state == States.INIT) || (invalidTradeTransaction.state == States.RESPONSE)) {
+                    invalidTradeTransaction.state = States.INVALID;
+                    invalidTradeTransaction.save();
+                    emailList.addAll(EmailSender.getBookExchangeInvalid(invalidTradeTransaction.owner, invalidTradeTransaction.recipient));
+                }
+            }
+            if (!emailList.isEmpty()) {
+                EmailSender.send(emailList);
+            }
+        }
     }
 
 }

app/views/book/createBook.scala.html
@@ -1,52 +1,52 @@
-@(bookForm: Form[Book], navigation: String)
+@(book: Book,  bookForm: Form[Book], navigation: String)
 @import helper._
 @main("Buch hinzufuegen", navigation) {
 
-	<div class="isbn-container center-block">	
-		@helper.form(action = routes.BookController.createBook()){
-			
-			@if(bookForm.hasGlobalErrors) {
-				<p class="error">@bookForm.globalError.message</p>
-			}
-				@inputText(
-					bookForm("title"),
-					'_label -> "Titel *",
-					'class -> "form-control input-sm"
-				)
-				@inputText(
-					bookForm("author"),
-					'_label -> "Autor *",
-					'class -> "form-control input-sm"
-				)
-				@inputText(
-					bookForm("isbn"),
-					'_label -> "ISBN *",
-					'class -> "form-control input-sm"
-				)
-				@inputText(
-					bookForm("year"),
-					'_label -> "Erscheinungsjahr *",
-					'class -> "form-control input-sm"
-				)
-				@inputText(
-					bookForm("coverUrl"),
-					'_label -> "Cover-URL *",
-					'class -> "form-control input-sm"
-				)
-				@textarea(
-					bookForm("comment"),
-					'_label -> "Kommentar",
-					'rows -> 4,
-					'class -> "form-control input-sm"
-				)		
-				<input type="submit" class="btn btn-primary" value="Buch anlegen">
-				<a href="@routes.BookController.addBook()" class="btn btn-default pull-right">Abbrechen</a>
-				<br />
-				<p class="form-hint">
-					* Pflichtfelder
-				</p>
+    <div class="isbn-container center-block">
+        @helper.form(action = routes.BookController.createBook()){
+            
+            @if(bookForm.hasGlobalErrors) {
+                <p class="error">@bookForm.globalError.message</p>
+            }
+                @inputText(
+                    bookForm("title"),
+                    '_label -> "Titel *",
+                    'class -> "form-control input-sm"
+                )
+                @inputText(
+                    bookForm("author"),
+                    '_label -> "Autor *",
+                    'class -> "form-control input-sm"
+                )
+                @inputText(
+                    bookForm("isbn"),
+                    '_label -> "ISBN *",
+                    'class -> "form-control input-sm"
+                )
+                @inputText(
+                    bookForm("year"),
+                    '_label -> "Erscheinungsjahr *",
+                    'class -> "form-control input-sm"
+                )
+                @inputText(
+                    bookForm("coverUrl"),
+                    '_label -> "Cover-URL *",
+                    'class -> "form-control input-sm"
+                )
+                @textarea(
+                    bookForm("comment"),
+                    '_label -> "Kommentar",
+                    'rows -> 4,
+                    'class -> "form-control input-sm"
+                )
+                <input type="submit" class="btn btn-primary" value="Buch anlegen">
+                <a href="@routes.BookController.addBook()" class="btn btn-default pull-right">Abbrechen</a>
+                <br />
+                <p class="form-hint">
+                    * Pflichtfelder
+                </p>
 
-		}
-	</div>
+        }
+    </div>
 
 }

commit 9077b158fa89531e84548052989b45a6f910cf5f

Fix form validationi, was broken since 71200e4e99c0a9faad10197906c391106ea2ed79

app/controllers/BookController.java
@@ -58,7 +58,7 @@ public final class BookController extends Controller {
         filledForm.errors().remove("tradeable");
         
         if(filledForm.hasErrors()) {
-            return badRequest(views.html.book.createBook.render(filledForm.get(), filledForm, navigation));
+            return badRequest(views.html.book.createBook.render(new Book(), filledForm, navigation));
         } else {
             
             final SimpleDateFormat formatter = new SimpleDateFormat( "yyyy-MM-dd" );